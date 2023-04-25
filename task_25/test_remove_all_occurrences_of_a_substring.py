33948711df12fcc97cbc5ddf7533601e45b4b939
aidarhaertdinov <aidarhaertdinov02@gmail.com>
2023-04-25 23:43:55 +0500

    add tasks and tests

---
+++ task_25/test_remove_all_occurrences_of_a_substring.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, main
+from task_25.remove_all_occurrences_of_a_substring import Solution
+
+
+class RemoveAllOccurrencesTest(TestCase):
+
+    def test_one_positive(self):
+        self.assertEqual(Solution.removeOccurrences(self, s="daabcbaabcbc", part="abc"),
+                         "dab")
+
+
+    def test_two_positive(self):
+        self.assertEqual(Solution.removeOccurrences(self, s="axxxxyyyyb", part="xy"),
+                         "ab")
+
+
+    def test_negative(self):
+        self.assertNotEqual(Solution.removeOccurrences(self, s="xciacia", part="ci"), "xia")
+
+
+
+if __name__ == '__main__':
+    main()
